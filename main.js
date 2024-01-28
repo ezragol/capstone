21a071f287e444e8c4425cbe9aa276ea16421157
Ezra Goldner <24goldnere@brooklinek12.org>
2024-01-28 11:25:39 -0500

    really fixing link issues

--- main.js
+++
@@ -1,9 +0,0 @@
-const links = document.querySelectorAll("a");
-const { host } = location;
-if (host == "ezragol.github.io") {
-    for (const link of links) {
-        const href = link.getAttribute("href");
-        if (href)
-            link.setAttribute("href", "/capstone" + href);
-    }
-}
