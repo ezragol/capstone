8de2a10870327816768f72eb0b404c978cdc2ec1
ezragol <ezgoldner@gmail.com>
2024-02-16 12:38:01 -0500

    adding more recordings

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 // audio redo...
+function playAudio(id) {
+    const audio = document.querySelector("#" + id);
+    audio.play();
+}
+
+function pauseAudio(id) {
+    const audio = document.querySelector("#" + id);
+    audio.pause();
+}
